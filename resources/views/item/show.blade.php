5bb3136184019811a53d509f02262bff743b613d
テイウンサツ <chengyunce@gmail.com>
2023-12-01 10:56:28 +0900

    update

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h2>商品情報</h2>
+                    <h3>商品名</h3>
+                    <p>
+                        {{ $item }}
+                    </p>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,22 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// HomeController
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/search', [HomeController::class, 'search'])->name('search');
+
+// ItemController
+Route::get('/item/', [ItemController::class, 'index'])->name('item.index');
+Route::get('/item/create', [ItemController::class, 'create'])->name('item.create');
+Route::post('/item/store', [ItemController::class, 'store'])->name('item.store');
+Route::get('/item/{id}', [ItemController::class, 'show'])->name('item.show');
 
 Route::get('/', function () {
+    // resources/views/welcome.blade.php ビューが表示
     return view('welcome');
-});
-
-Route::get('/about', function () {
-    return "About Page!";
-});
-
-Route::get('/item/{id}', function ($id) {
-    $message = "商品IDは{$id}";
-    return $message;
-});
-
-Route::get('/dp/{id}', function ($id) {
-    $message = "商品IDは{$id}";
-    return $message;
-});
-
-Route::get('/search', function (Request $request) {
-   //dd($request);
-   $keyword = $request -> q;
-   $message = "キーワードは{$keyword}です。";
-   return $message;
 });
 
 Route::get('/dashboard', function () {
