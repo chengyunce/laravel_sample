961ca39975a6186e94781166b5a8a3c0bb708a58
テイウンサツ <chengyunce@gmail.com>
2023-12-08 13:18:28 +0900

    update

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -19,4 +19,23 @@ class ItemRequest extends FormRequest
             'price' => ['required', 'integer', 'min:0', 'max:100000'],
         ];
     }
+
+    public function attributes()
+    {
+        return [
+            'name' => __('messages.item_name'),
+            'price' => __('messages.price'),
+        ];
+    }
+
+    //エラーメッセージ
+    public function messages()
+    {
+        return [
+            // 'name.required' => '商品名を入力してください',
+            // 'price.required' => '価格を入力してください',
+            // 'price.integer' => '価格を数値で入力してください',
+            // 'price.min' => '価格を数値で入力してください',
+        ];
+    }
 }
